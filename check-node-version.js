0327f605f2007488fcd2bc056c92ba083fd278e3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-14 13:20:04 -0700

    chore: run script from postinstall-node-version-check

--- check-node-version.js
+++
@@ -1,5 +0,0 @@
-var version = process.version;
-
-if (version && parseInt(version.substring(1, version.indexOf("."))) < 10) {
-  console.warn(`The Node.js version ${version} is no longer supported.`);
-}
